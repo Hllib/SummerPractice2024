e179ea45b455efab922f6cb5b876ef30751cb83f
Hlib <hlib.monastyrov@gmail.com>
2024-08-04 12:24:19 +0300

    Setup new camera positions

---
+++ Assets/Scripts/GizmosDrawer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace HlStudio
+{
+    public class GizmosDrawer : MonoBehaviour
+    {
+        [Conditional("UNITY_EDITOR")]
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawWireSphere(transform.position, 2f);
+        }
+    }
+}
